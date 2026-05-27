ecc9a7da3b78f2b76598809a78b50d127ee34650
miroslav-pavlov <94829772+miroslav-pavlov@users.noreply.github.com>
2026-05-27 21:33:59 +0300

    watermark hiding fixed, animation removed, header is now white

--- src/menu.js
+++ src/menu.js
@@ -2,13 +2,16 @@
 function createFloatingMenu(isOnMobile = false) {
     createMenu(isOnMobile);
 
-    window.onExtensionActivated = function () {
+    window.onExtensionActivated = function () {};
+    window.onSiteStartTestBtnClicked = function () {
         try {
             const watermark = document.getElementById("tt-status");
             if (watermark) {
                 watermark.style.display = "none";
             }
-        } catch {}
+        } catch(e){
+            console.error("Error hiding watermark:", e);
+        }
     };
 
     window.fullNameSubmitted = function (enteredName, enteredNumber) {
@@ -16,6 +19,5 @@ function createFloatingMenu(isOnMobile = false) {
         window.classNumber = enteredNumber;
     };
 
-    window.onSiteStartTestBtnClicked = function () {};
     window.onSiteStageReset = function () {};
 }
